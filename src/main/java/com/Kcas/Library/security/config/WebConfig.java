5318d018511294ae78a24d377a7e064528d16329
Olexlant <zgn5bx3q7>
2023-08-12 22:22:33 +0300

    project update

--- src/main/java/com/Kcas/Library/security/config/WebConfig.java
+++ src/main/java/com/Kcas/Library/security/config/WebConfig.java
@@ -1,50 +1,19 @@
 package com.Kcas.Library.security.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.TemplateEngine;
 
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-
-    }
+@EnableWebMvc
+public class WebConfig {
 
     @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource resource =
-                new ResourceBundleMessageSource();
-        resource.setBasename("messages");
-        resource.setDefaultEncoding("UTF-8");
-        return resource;
-    }
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.forLanguageTag("ua"));
-        return slr;
-    }
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
+    public TemplateEngine templateEngine() {
+        return new TemplateEngine();
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-    }
 
 }
